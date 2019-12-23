f67d1307fa73d8ab403e56fb5ad83543639f3ac4
gourav02m <46623967+gourav02m@users.noreply.github.com>
2019-12-23 18:28:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, {Component} from 'react';
+import React, {Component, lazy, Suspense } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 
-import Displaysrc from "./component/Displaysrc";
+//import Displaysrc from "./component/Displaysrc";
+const Displaysrc =lazy(() => import("./component/Displaysrc"));
 
 class App extends Component{
   render(){
   return (
-    <Router>
-    <div className="App">
-
-
-
-
    
-    </div>
+    <Router>
+    <div>
+    <div><h1>Names,Emails,Websites of Company:-</h1></div>
+     <Suspense fallback={<div>Please Wait.......</div>}>
     <Route path="/" exact component={Displaysrc} />
+    </Suspense>
     
+   </div>
+   </Router>
+   
 
-    
-    </Router>
   );
 }
 }
