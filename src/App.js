38a70b71d60d39472fbf72606f6bd8da60782959
gourav02m <46623967+gourav02m@users.noreply.github.com>
2019-12-25 17:10:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, {Component, lazy, Suspense } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router ,Route} from 'react-router-dom';
 
 
-//import Displaysrc from "./component/Displaysrc";
-const Displaysrc =lazy(() => import("./component/Displaysrc"));
+import Employee_details from './component/Employee_details';
+const Displaysrc =lazy(() => import('./component/Displaysrc'));
 
 class App extends Component{
   render(){
@@ -11,10 +11,12 @@ class App extends Component{
    
     <Router>
     <div>
-    <div><h1>Names,Emails,Websites of Company:-</h1></div>
+    <div><h1>Employee Names,Employee Age of Company:-</h1></div>
      <Suspense fallback={<div>Please Wait.......</div>}>
     <Route path="/" exact component={Displaysrc} />
     </Suspense>
+    <Route name="/create" path="/:testvalue" handler={Employee_details} exact component={Employee_details} render={(props) => <Employee_details Displaysrc={Displaysrc} {...props} /> }/>
+    
     
    </div>
    </Router>
@@ -25,3 +27,4 @@ class App extends Component{
 }
 
 export default App;
+
